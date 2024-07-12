8e45954d1d0c68acdfe7df853b6827fb46b6705a
Gabriel Gomes <gabriel.fgomes@gmail.com>
2024-07-12 10:53:37 -0300

    Formulário com a versão 2 funcionando

---
+++ lib/controller/user_controller_v2.dart
@@ -0,0 +1,65 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import '../models/user.dart';
+
+/*O código comentado é deixado de propósito para o usuário ver o que não é necessário.*/
+class UserController extends GetxController {
+  var nameController = TextEditingController().obs;
+  Rx<String?> gender = null.obs;
+  var userList = <User>[].obs;
+
+  // Campos de validação
+  // var nameError = ''.obs;
+  // var genderError = ''.obs;
+
+  void setName(String name) {
+    nameController.value.text = name;
+    //validateName();
+  }
+
+  void setGender(String? selectedGender) {
+    gender.value = selectedGender;
+    //validateGender();
+  }
+
+  // void validateName() {
+  //   if (nameController.value.text.isEmpty) {
+  //     nameError.value = 'Nome é obrigatório';
+  //   } else {
+  //     nameError.value = '';
+  //   }
+  // }
+
+  // void validateGender() {
+  //   if (gender.value!.isEmpty) {
+  //     genderError.value = 'Gênero é obrigatório';
+  //   } else {
+  //     genderError.value = '';
+  //   }
+  // }
+
+  // bool validateForm() {
+  //   validateName();
+  //   validateGender();
+  //   return nameError.value.isEmpty && genderError.value.isEmpty;
+  // }
+
+  void saveUser() {
+    // if (validateForm()) {
+    //   userList.add(User(name: nameController.value.text, gender: gender.value));
+    //   clearUser();
+    // }
+
+    userList.add(User(name: nameController.value.text, gender: gender.value));
+    clearUser();
+  }
+
+  void clearUser() {
+    nameController.value.clear();
+    gender.value = null;
+  }
+
+  void deleteUser(int index) {
+    userList.removeAt(index);
+  }
+}
